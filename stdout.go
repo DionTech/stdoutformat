67e28c07fb6b3f4c4494b0c713538b464fac4d6f
Daniel Koch <daniel.koch@publicare.dem>
2021-01-09 20:46:42 +0100

    some really basic stuff t the moment

---
+++ stdout.go
@@ -0,0 +1,26 @@
+package stdoutformat
+
+import "fmt"
+
+const (
+	InfoColor    = "\033[1;34m%s\033[0m"
+	NoticeColor  = "\033[1;36m%s\033[0m"
+	WarningColor = "\033[1;33m%s\033[0m"
+	ErrorColor   = "\033[1;31m%s\033[0m"
+	DebugColor   = "\033[0;36m%s\033[0m"
+)
+
+func Error(err error) {
+	fmt.Printf(ErrorColor+"\n", err)
+}
+
+func Fatalf(format string, a ...interface{}) {
+	str := fmt.Sprintf(format, a)
+	fmt.Printf(ErrorColor+"\n", str)
+	panic("")
+}
+
+func Printf(format string, a ...interface{}) {
+	str := fmt.Sprintf(format, a)
+	fmt.Printf(ErrorColor+"\n", str)
+}
